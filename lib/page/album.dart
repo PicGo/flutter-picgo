72da0ad1d8d5f2a4cb85a1893a0952ebbf873b1c
hackycy <qa894178522@qq.com>
2020-06-15 17:37:15 +0800

    feature：配置ImagePicker所需配置

--- lib/page/album.dart
+++ lib/page/album.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AlbumPage extends StatelessWidget {
 
   static const routeName = '/album';
+
+  final picker = ImagePicker();
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +15,21 @@ class AlbumPage extends StatelessWidget {
         title: Text('相册'),
         centerTitle: true,
       ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.file_upload),
+        onPressed: () {
+          getImage();
+        },
+      ),
     );
+  }
+
+  void getImage() async {
+    try {
+      final pickedFile = await picker.getImage(source: ImageSource.gallery);
+    } catch (e) {
+      print('报错$e');
+    }
   }
 
 }
