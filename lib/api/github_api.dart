1a41e037f5fea643c942e8c6f31ba9ed8bee4682
hackycy <qa894178522@qq.com>
2020-06-17 10:57:06 +0800

    feature：封装api请求类

---
+++ lib/api/github_api.dart
@@ -0,0 +1,5 @@
+class GithubApi {
+
+  static const String BASE_URL = 'https://api.github.com/';
+
+}
---
+++ lib/utils/net.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+// import 'dart:io';
+// import 'package:dio/dio.dart';
+
+Map<String, dynamic> optHeader = {
+  'accept-language': 'zh-cn',
+  'content-type': 'application/json'
+};
+
+class NetUtil {
+
+  static Future get(String url, [Map<String, dynamic> params]) async {
+    
+  }
+
+}
