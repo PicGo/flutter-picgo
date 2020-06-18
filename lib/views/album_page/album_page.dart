4bda276eb3ee70342106ac6d20177291c2cb9052
hackycy <qa894178522@qq.com>
2020-06-18 18:18:21 +0800

    feature：编写使用ImagePicker获取图片到对话框预览

--- lib/views/album_page/album_page.dart
+++ lib/views/album_page/album_page.dart
@@ -1,8 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_picgo/components/picker_image.dart';
+import 'package:flutter_picgo/model/uploaded.dart';
+import 'package:flutter_picgo/views/album_page/album_page_presenter.dart';
 import 'package:image_picker/image_picker.dart';
 
-class AlbumPage extends StatelessWidget {
+class AlbumPage extends StatefulWidget {
+  @override
+  _AlbumPageState createState() => _AlbumPageState();
+}
 
+class _AlbumPageState extends State<AlbumPage> implements AlbumPageContract {
   final picker = ImagePicker();
 
   @override
@@ -13,7 +22,7 @@ class AlbumPage extends StatelessWidget {
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.file_upload),
+        child: Icon(IconData(0xe639, fontFamily: 'iconfont')),
         onPressed: () {
           getImage();
         },
@@ -24,9 +33,18 @@ class AlbumPage extends StatelessWidget {
   void getImage() async {
     try {
       final pickedFile = await picker.getImage(source: ImageSource.gallery);
+      await showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (context) {
+            return PickerImageDialog('dasdasd.png', pickedFile.path);
+          });
+      File(pickedFile.path);
     } catch (e) {
       print('报错$e');
     }
   }
 
+  @override
+  void loadUploadedImages(List<Uploaded> uploadeds) {}
 }
---
+++ lib/views/album_page/album_page_presenter.dart
@@ -0,0 +1,27 @@
+import 'dart:io';
+import 'package:flutter_picgo/model/uploaded.dart';
+import 'package:flutter_picgo/resources/table_name_keys.dart';
+import 'package:flutter_picgo/utils/sql.dart';
+
+abstract class AlbumPageContract {
+
+  void loadUploadedImages(List<Uploaded> uploadeds);
+
+}
+
+class AlbumPagePresenter {
+
+  AlbumPageContract _view;
+
+  AlbumPagePresenter(this._view);
+
+  doLoadUploadedImages() {
+    var sql = Sql.setTable(TABLE_NAME_UPLOADED);
+
+  }
+
+  doUploadImage(File imgFile) {
+    
+  }
+
+}
