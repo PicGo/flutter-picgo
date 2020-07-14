afdb4313f91c377237d38893201f13519f51f733
hackycy <qa894178522@qq.com>
2020-07-14 12:07:35 +0800

    styles：UploadStrategyFactory add cache

--- lib/utils/strategy/upload_strategy_factory.dart
+++ lib/utils/strategy/upload_strategy_factory.dart
@@ -8,22 +8,26 @@ import 'package:flutter_picgo/utils/strategy/impl/smms_image_upload.dart';
 import 'package:flutter_picgo/utils/strings.dart';
 
 class UploadStrategyFactory {
+  static Map<String, ImageUploadStrategy> cache = {};
+
   /// UploadStrategy工厂类，负责创建UploadStrategy
   static ImageUploadStrategy getUploadStrategy(String type) {
     if (isBlank(type)) {
       throw new NullThrownError();
     }
-    if (type == PBTypeKeys.github) {
-      return new GithubImageUpload();
-    } else if (type == PBTypeKeys.smms) {
-      return new SMMSImageUpload();
-    } else if (type == PBTypeKeys.gitee) {
-      return new GiteeImageUpload();
-    } else if (type == PBTypeKeys.qiniu) {
-      return new QiniuImageUpload();
-    } else if (type == PBTypeKeys.aliyun) {
-      return new AliyunImageUpload();
+    if (cache[type] == null) {
+      if (type == PBTypeKeys.github) {
+        cache[type] = new GithubImageUpload();
+      } else if (type == PBTypeKeys.smms) {
+        cache[type] = new SMMSImageUpload();
+      } else if (type == PBTypeKeys.gitee) {
+        cache[type] = new GiteeImageUpload();
+      } else if (type == PBTypeKeys.qiniu) {
+        cache[type] = new QiniuImageUpload();
+      } else if (type == PBTypeKeys.aliyun) {
+        cache[type] = new AliyunImageUpload();
+      }
     }
-    return null;
+    return cache[type];
   }
 }
