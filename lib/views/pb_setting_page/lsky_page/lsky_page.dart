f52e15a103aabe5a3f385275a27beb75ecbcc469
hackycy <qa894178522@qq.com>
2020-07-23 11:55:02 +0800

    feature：complete lsky config page

--- lib/views/pb_setting_page/lsky_page/lsky_page.dart
+++ lib/views/pb_setting_page/lsky_page/lsky_page.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_picgo/api/lsky_api.dart';
 import 'package:flutter_picgo/model/config.dart';
 import 'package:flutter_picgo/model/lsky_config.dart';
 import 'package:flutter_picgo/resources/pb_type_keys.dart';
 import 'package:flutter_picgo/utils/strings.dart';
 import 'package:flutter_picgo/views/pb_setting_page/base_pb_page_state.dart';
+import 'package:toast/toast.dart';
 
 class LskyPage extends StatefulWidget {
   _LskyPageState createState() => _LskyPageState();
@@ -38,7 +40,19 @@ class _LskyPageState extends BasePBSettingPageState<LskyPage> {
       } else if (key == 'token') {
         config = Config(
             label: '设定Token',
-            placeholder: 'token',
+            placeholder: 'Token',
+            needValidate: false,
+            value: value);
+      } else if (key == 'email') {
+        config = Config(
+            label: '设定邮箱',
+            placeholder: 'Email',
+            needValidate: true,
+            value: value);
+      } else if (key == 'password') {
+        config = Config(
+            label: '设定密码',
+            placeholder: '设定密码',
             needValidate: true,
             value: value);
       }
@@ -49,5 +63,31 @@ class _LskyPageState extends BasePBSettingPageState<LskyPage> {
   }
 
   @override
+  String get tip => '点击保存会自动获取Token，如果已填写字段则不会自动生成';
+
+  @override
   String get pbType => PBTypeKeys.lsky;
+
+  @override
+  save() async {
+    if (isBlank(controllers['token'].text.trim())) {
+      // 如果Token为空，则自动获取
+      String email = controllers['email'].value.text.trim();
+      String pwd = controllers['password'].value.text.trim();
+      String host = controllers['host'].value.text.trim();
+      var result = await LskyApi.token(email, pwd, host);
+      if (result["code"] == 200) {
+        // controllers["token"].text = '${result["data"]["token"]}';
+        setState(() {
+          configs[3].value = '${result["data"]["token"]}';
+
+          super.save();
+        });
+      } else {
+        Toast.show('Token获取失败，请检查配置', context);
+      }
+    } else {
+      return super.save();
+    }
+  }
 }
